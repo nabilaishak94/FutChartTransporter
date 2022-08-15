f6e7cc2bc1c70b73ed5bbe4b5fc2942c2caff2a6
nabilaishak94 <64452640+nabilaishak94@users.noreply.github.com>
2022-08-15 15:54:30 +0800

    Add files via upload

---
+++ Common/LoggerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace FutChartTransporter_DotCore.Common
+{
+    public class LoggerService
+    {
+        private readonly ILogger<LoggerService> _logger;
+
+        public LoggerService(ILogger<LoggerService> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
